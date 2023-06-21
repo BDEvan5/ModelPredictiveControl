ee298732f8a3cdf443559d6d6252f15dcfeaaa41
Benjamin <benjaminevans316@gmail.com>
2023-06-21 12:02:18 +0200

    temp stage for devel

--- FO/MPCC_devel.py
+++ FO/MPCC_devel.py
@@ -96,15 +96,25 @@ def update_state(x, u, dt):
 
 class PlannerMPC:
     def __init__(self, path, dt, N):
-        self.path = ReferencePath()
+        self.rp = ReferencePath()
         self.dt = dt
         self.N = N # number of steps to predict
         
         self.nx = 4
         self.nu = 2
         self.T_V = self.nx + self.nu
-        self.u_lim = 1.2
         
+        self.x_min, self.y_min = np.min(self.rp.path, axis=0)
+        self.psi_min, self.s_min = -np.pi, 0
+        self.x_max, self.y_max = np.max(self.rp.path, axis=0)
+        self.psi_max, self.s_max = np.pi, self.rp.s_track[-1]
+
+        self.theta_min, self.p_min = -0.4, 0
+        self.theta_max, self.p_max = 0.4, 4
+
+        self.u0 = np.zeros((self.N, self.nu))
+        self.X0 = np.zeros((self.N + 1, self.nx))
+
     def generate_reference_path(self, x0):
         nearest_idx = np.argmin(np.linalg.norm(self.path - x0[:2], axis=1))
         
@@ -123,7 +133,7 @@ class PlannerMPC:
         
         return u_bar[0] # return the first control action
         
-    def generate_optimal_path(self, x0_in, x_ref, u_init):
+    def generate_optimal_path(self, x0_in, u_init):
         # States
         x = ca.MX.sym('x')
         y = ca.MX.sym('y')
@@ -140,6 +150,7 @@ class PlannerMPC:
         self.f = ca.Function('f', [states, controls], [rhs])  # nonlinear mapping function f(x,u)
         self.U = ca.MX.sym('U', self.nu, self.N)
         self.X = ca.MX.sym('X', self.nx, (self.N + 1))
+        self.P = ca.MX.sym('P', self.nx + 2 * self.N) # init state and boundaries of the reference path
 
         self.Q = ca.MX.zeros(2, 2)
         self.Q[0, 0] = 50  # cross track error
@@ -148,57 +159,128 @@ class PlannerMPC:
         self.obj = 0  # Objective function
         self.g = []  # constraints vector
 
-        x = ca.SX.sym('x', self.nx, self.N+1)
-        u = ca.SX.sym('u', self.nu, self.N)
-        
-        J = ca.sumsqr(x[:2, :-1] - x_ref[:, :-1])
-        
-        g = []
+        st = self.X[:, 0]  # initial state
+        self.g = ca.vertcat(self.g, st - x0_in)  # initial condition constraints
         for k in range(self.N):
-            x_next = x[:,k] + f(x[:,k], u[:,k])*self.dt
-            g.append(x_next - x[:,k+1])
+            st = self.X[:, k]
+            st_next = self.X[:, k + 1]
+            con = self.U[:, k]
+            t_angle = self.rp.angle_lut_t(st_next[3])
+            ref_x, ref_y = self.rp.center_lut_x(st_next[3]), self.rp.center_lut_y(st_next[3])
+            #Contouring error
+            e_c = ca.sin(t_angle) * (st_next[0] - ref_x) - ca.cos(t_angle) * (st_next[1] - ref_y)
+            #Lag error
+            e_l = -ca.cos(t_angle) * (st_next[0] - ref_x) - ca.sin(t_angle) * (st_next[1] - ref_y)
+            error = ca.vertcat(e_c, e_l)
 
-        initial_constraint = x[:,0] - x0_in 
-        g.append(initial_constraint)
+            self.obj = self.obj + ca.mtimes(ca.mtimes(error.T, self.Q), error) #! add a cost for the centerline velocity
 
-        x_init = [x0_in]
-        for i in range(1, 1+self.N):
-            x_init.append(x_init[i-1] + f(x_init[i-1], [u_init[i-1]])*self.dt)
-        x_init.append(u_init)
-        x_init = ca.vertcat(*x_init)
-        
-        lbx = [-ca.inf] * self.nx * (self.N+1) + [-self.u_lim] * self.N
-        ubx = [ca.inf] * self.nx * (self.N+1) + [self.u_lim] * self.N
-        
-        x_nlp = ca.vertcat(x.reshape((-1, 1)), u.reshape((-1, 1)))
-        g_nlp = ca.vertcat(*g)
-        nlp = {'x': x_nlp,
-            'f': J,
-            'g': g_nlp}
+            k1 = self.f(st, con)
+            st_next_euler = st + (self.dt * k1)
+            self.g = ca.vertcat(self.g, st_next - st_next_euler)  # compute constraints
 
-        opts = {'ipopt': {'print_level': 2},
-                'print_time': False}
-        solver = ca.nlpsol('solver', 'ipopt', nlp, opts)
-        sol = solver(x0=x_init, lbx=lbx, ubx=ubx, lbg=0, ubg=0)
+            # path boundary constraints
+            self.g = ca.vertcat(self.g, self.P[self.nx + 2 * k] * st_next[0] - self.P[self.nx + 2 * k + 1] * st_next[1])  # LB<=ax-by<=UB  --represents half space planes
 
-        x_bar = np.array(sol['x'][:self.nx*(self.N+1)].reshape((self.nx, self.N+1)))
-        u_bar = sol['x'][self.nx*(self.N+1):]
-        
-        if VERBOSE:
-            plt.figure(1)
-            plt.clf()
-            plt.plot(x_ref[0, :], x_ref[1, :], label="x_ref")
-            init_states = np.array(x_init[:self.nx*(self.N+1)].reshape((self.nx, self.N+1)))
-            plt.plot(init_states[0, :], init_states[1, :], label="Estimated States (x0)")
-            plt.plot(x_bar[0, :], x_bar[1, :], label="Solution States (x_bar)") 
+
+        # setup solver with bounds, and initial value
+        p = np.zeros(self.nx + 2 * self.N)
+        p[:self.nx] = x0_in
+
+        right_points, left_points = self.get_path_constraints_points(self.X0)
+        for k in range(self.N):  # set the reference controls and path boundary conditions to track
+            delta_x_path = right_points[k, 0] - left_points[k, 0]
+            delta_y_path = right_points[k, 1] - left_points[k, 1]
+            p[self.nx + 2 * k:self.nx + 2 * k + 2] = [-delta_x_path, delta_y_path]
+            up_bound = max(-delta_x_path * right_points[k, 0] - delta_y_path * right_points[k, 1],
+                           -delta_x_path * left_points[k, 0] - delta_y_path * left_points[k, 1])
+            low_bound = min(-delta_x_path * right_points[k, 0] - delta_y_path * right_points[k, 1],
+                            -delta_x_path * left_points[k, 0] - delta_y_path * left_points[k, 1])
             
-            plt.title(f"States and Reference Path ")
-            plt.legend()
-            
-            plt.pause(0.01)
+            self.lbg[self.nx - 1 + (self.nx + 1) * (k + 1), 0] = low_bound # check this, there could be an error
+            self.ubg[self.nx - 1 + (self.nx + 1) * (k + 1), 0] = up_bound
+
+        self.construct_warm_start_soln(x0_in)
+        x_init = ca.vertcat(ca.reshape(self.X0.T, self.nx * (self.N + 1), 1),
+                         ca.reshape(self.u0.T, self.nx * self.N, 1))
+        OPT_variables = vertcat(reshape(self.X, self.n_states * (self.N + 1), 1),
+                                reshape(self.U, self.n_controls * self.N, 1))
         
-        return np.array(u_bar)[:, 0]
+        opts = {}
+        opts["ipopt"] = {}
+        opts["ipopt"]["max_iter"] = 2000
+        opts["ipopt"]["print_level"] = 0
+        nlp_prob = {'f': self.obj, 'x': OPT_variables, 'g': self.g, 'p': self.P}
+        self.solver = ca.nlpsol('solver', 'ipopt', nlp_prob, opts)
+        sol = self.solver(x0=x_init, lbx=self.lbx, ubx=self.ubx, lbg=self.lbg, ubg=self.ubg, p=p)
+
+
+        # Get state and control solution
+        self.X0 = ca.reshape(sol['x'][0:self.nx * (self.N + 1)], self.nu, self.N + 1).T  # get soln trajectory
+        u = ca.reshape(sol['x'][self.nx * (self.N + 1):], self.nu, self.N).T  # get controls solution
+
+        # Get the first control
+        con_first = u[0, :].T
+        trajectory = self.X0.full()  # size is (N+1,n_states)
+        inputs = u.full()
+
+        # Shift trajectory and control solution to initialize the next step
+        self.X0 = ca.vertcat(self.X0[1:, :], self.X0[self.X0.size1() - 1, :])
+        self.u0 = ca.vertcat(u[1:, :], u[u.size1() - 1, :])
+        # return con_first, trajectory, inputs
+
+        return con_first
         
+        # return np.array(u_bar)[:, 0]
+    
+    def construct_warm_start_soln(self, initial_state):
+        # Construct an initial estimated solution to warm start the optimization problem with valid path constraints
+        #! this will break for multiple laps
+        # if initial_state[3] >= self.arc_lengths_orig_l:
+        #     initial_state[3] -= self.arc_lengths_orig_l
+        p_initial = 2
+        initial_state[2] = self.rp.angle_lut_t(initial_state[3])
+        self.X0[0, :] = initial_state
+        for k in range(1, self.N + 1):
+            s_next = self.X0[k - 1, 3] + p_initial * self.dt
+            psi_next = self.rp.angle_lut_t(s_next)
+            x_next, y_next = self.get_point_at_centerline(s_next)
+            self.X0[k, :] = [x_next, y_next, psi_next, s_next]
+
+    def get_point_at_centerline(self, s):
+        x, y = self.rp.center_lut_xy(s), self.rp.center_lut_y(s)
+        return x, y
+
+    def init_constraints(self):
+        '''Initialize constraints for states, dynamic model state transitions and control inputs of the system'''
+        self.lbg = np.zeros((self.nx * (self.N + 1) + self.N, 1))
+        self.ubg = np.zeros((self.nx * (self.N + 1) + self.N, 1))
+        self.lbx = np.zeros((self.nx + (self.nx + self.nu) * self.N, 1))
+        self.ubx = np.zeros((self.nx + (self.nx + self.nu) * self.N, 1))
+        # Upper and lower bounds for the state optimization variables
+        for k in range(self.N + 1):
+            self.lbx[self.nx * k:self.nx * (k + 1), 0] = np.array(
+                [[self.x_min, self.y_min, self.psi_min, self.s_min]])
+            self.ubx[self.nx * k:self.nx * (k + 1), 0] = np.array(
+                [[self.x_max, self.y_max, self.psi_max, self.s_max]])
+        state_count = self.nx * (self.N + 1)
+        # Upper and lower bounds for the control optimization variables
+        for k in range(self.N):
+            self.lbx[state_count:state_count + self.nu, 0] = np.array(
+                [[self.theta_min, self.p_min]])  # v and theta lower bound
+            self.ubx[state_count:state_count + self.nu, 0] = np.array(
+                [[self.theta_max, self.p_max]])  # v and theta upper bound
+            state_count += self.nu
+
+    def get_path_constraints_points(self, prev_soln):
+        right_points = np.zeros((self.N, 2))
+        left_points = np.zeros((self.N, 2))
+        for k in range(1, self.N + 1):
+            right_points[k - 1, :] = [self.rp.right_lut_x(prev_soln[k, 3]),
+                                      self.rp.right_lut_y(prev_soln[k, 3])]  # Right boundary
+            left_points[k - 1, :] = [self.rp.left_lut_x(prev_soln[k, 3]),
+                                     self.rp.left_lut_y(prev_soln[k, 3])]  # Left boundary
+        return right_points, left_points
 
 def f(x, u):
     # define the dynamics as a casadi array
